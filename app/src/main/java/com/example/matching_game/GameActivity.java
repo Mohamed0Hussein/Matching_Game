cffcb5d1745aa0d37e8290ee1e5ce73e5f26ec59
Mohamed Hussein <mohamed.hussein.1.9.9.71.9.9.8@gmail.com>
2019-12-06 23:52:56 +0200

    fully functional
    need to add sounds

--- app/src/main/java/com/example/matching_game/GameActivity.java
+++ app/src/main/java/com/example/matching_game/GameActivity.java
@@ -9,6 +9,7 @@ import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -20,16 +21,24 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-    TextView player,score;
+    TextView player, score;
     int Score;
+    int pressed_card_1;
+    int pressed_card_2;
+    int pressed_pos_1;
+    int pressed_pos_2;
+    boolean[] opened_cards;
     Random rand;
     int[] cards;
     int[] choosed_cards;
     ImageView[] viewing_cards;
-    int[] pos;
-    int n1,n2,n3,n4;
-    public void fetchCards(){
-        pos = new int[8];
+    //    int[] pos;
+    int n1, n2, n3, n4;
+
+    public void fetchCards() {
+        pressed_card_1 = 0;
+        pressed_card_2 = 0;
+//        pos = new int[8];
         cards = new int[52];
         viewing_cards = new ImageView[8];
         viewing_cards[0] = findViewById(R.id.imageView1);
@@ -96,13 +105,22 @@ public class GameActivity extends AppCompatActivity {
         cards[51] = (R.drawable.s13);
 
     }
-    public void StartPlaying(){
-        choosed_cards = new int[4];
-        n1=0;
-        n2=0;
-        n3=0;
-        n4=0;
-        for(int i=0;i<8;i++) {
+
+    public void StartPlaying() {
+        for(int i =0;i<8;i++) {
+            viewing_cards[i].setEnabled(true);
+            opened_cards[i] = false;
+        }
+        pressed_pos_1 = -1;
+        pressed_pos_2 = -1;
+        Score = 0;
+        score.setText("Score:" + Score);
+        choosed_cards = new int[8];
+        n1 = 0;
+        n2 = 0;
+        n3 = 0;
+        n4 = 0;
+        for (int i = 0; i < 8; i++) {
             viewing_cards[i].setImageResource(R.drawable.card_back);
             viewing_cards[i].setTag(i);
         }
@@ -111,32 +129,27 @@ public class GameActivity extends AppCompatActivity {
             n2 = rand.nextInt(52);
             n3 = rand.nextInt(52);
             n4 = rand.nextInt(52);
-        } while((n1 == n2) || (n1 == n3) || (n1 == n4) || (n2 == n3) || (n2 == n4) || (n3 == n4));
+        } while ((n1 == n2) || (n1 == n3) || (n1 == n4) || (n2 == n3) || (n2 == n4) || (n3 == n4));
 
 
         choosed_cards[0] = cards[n1];
         choosed_cards[1] = cards[n2];
         choosed_cards[2] = cards[n3];
         choosed_cards[3] = cards[n4];
+        choosed_cards[4] = cards[n1];
+        choosed_cards[5] = cards[n2];
+        choosed_cards[6] = cards[n3];
+        choosed_cards[7] = cards[n4];
 
-        Log.i("Choosed Card 1",String.valueOf((n1)));
-        Log.i("Choosed Card 2",String.valueOf((n2)));
-        Log.i("Choosed Card 3",String.valueOf((n3)));
-        Log.i("Choosed Card 4",String.valueOf((n4)));
+        shuffle(choosed_cards);
 
 
-
-        pos[0] = 0;
-        pos[1] = 0;
-        pos[2] = 1;
-        pos[3] = 1;
-        pos[4] = 2;
-        pos[5] = 2;
-        pos[6] = 3;
-        pos[7] = 3;
     }
 
-    public void CardPressed(final View v){
+    public void CardPressed(final View v) {
+        for(int i=0;i<8;i++){
+            viewing_cards[i].setEnabled(false);
+        }
         //viewing_cards[Integer.parseInt(v.getTag().toString())].setImageResource(choosed_cards[pos[Integer.parseInt(v.getTag().toString())]]);
         final ObjectAnimator oa1 = ObjectAnimator.ofFloat(viewing_cards[Integer.parseInt(v.getTag().toString())], "scaleX", 1f, 0f);
         final ObjectAnimator oa2 = ObjectAnimator.ofFloat(viewing_cards[Integer.parseInt(v.getTag().toString())], "scaleX", 0f, 1f);
@@ -146,25 +159,149 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
-                viewing_cards[Integer.parseInt(v.getTag().toString())].setImageResource(choosed_cards[pos[Integer.parseInt(v.getTag().toString())]]);
+                viewing_cards[Integer.parseInt(v.getTag().toString())].setImageResource(choosed_cards[Integer.parseInt(v.getTag().toString())]);
                 oa2.start();
             }
         });
         oa1.start();
+        if (pressed_card_1 == 0) {
+            pressed_card_1 = choosed_cards[Integer.parseInt(v.getTag().toString())];
+            pressed_pos_1=Integer.parseInt(v.getTag().toString());
+            viewing_cards[pressed_pos_1].setEnabled(false);
+            for(int i=0;i<8;i++){
+                if(i!=pressed_pos_1)
+                    viewing_cards[i].setEnabled(true);
+            }
+        } else if (pressed_card_2 == 0) {
+            pressed_card_2 = choosed_cards[Integer.parseInt(v.getTag().toString())];
+            pressed_pos_2=Integer.parseInt(v.getTag().toString());
+            viewing_cards[pressed_pos_2].setEnabled(false);
+
+            if (pressed_card_1 == pressed_card_2) {
+                Score++;
+                score.setText("Score: " + Score);
+                if (Score == 4) {
+                    score.setText("Press Reset to play");
+                }
+                opened_cards[pressed_pos_1] = true;
+                opened_cards[pressed_pos_2] = true;
+                viewing_cards[pressed_pos_2].setEnabled(false);
+                viewing_cards[pressed_pos_1].setEnabled(false);
+                for(int i=0;i<8;i++){
+                    if(i != pressed_pos_1 && i != pressed_pos_2)
+                        viewing_cards[i].setEnabled(true);
+                }
+                pressed_pos_1=-1;
+                pressed_pos_2=-1;
+                pressed_card_2=0;
+                pressed_card_1=0;
+
+
+
+
+            } else {
+                Score--;
+                score.setText("Score: "+Score);
+
+                new CountDownTimer(1000, 100) {
+                    @Override
+                    public void onTick(long l) {
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        for(int i=0;i<8;i++){
+                            viewing_cards[i].setEnabled(false);
+                        }
+                        final ObjectAnimator oaaa1 = ObjectAnimator.ofFloat(viewing_cards[pressed_pos_2], "scaleX", 1f, 0f);
+                        final ObjectAnimator oaaa2 = ObjectAnimator.ofFloat(viewing_cards[pressed_pos_2], "scaleX", 0f, 1f);
+                        oaaa1.setInterpolator(new DecelerateInterpolator());
+                        oaaa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                        oaaa1.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                viewing_cards[pressed_pos_2].setImageResource(R.drawable.card_back);
+                                oaaa2.start();
+                            }
+                        });
+                        oaaa1.start();
+                        final ObjectAnimator oaa1 = ObjectAnimator.ofFloat(viewing_cards[pressed_pos_1], "scaleX", 1f, 0f);
+                        final ObjectAnimator oaa2 = ObjectAnimator.ofFloat(viewing_cards[pressed_pos_1], "scaleX", 0f, 1f);
+                        oaa1.setInterpolator(new DecelerateInterpolator());
+                        oaa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                        oaa1.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                viewing_cards[pressed_pos_1].setImageResource(R.drawable.card_back);
+                                oaa2.start();
+                            }
+                        });
+                        oaa1.start();
+
+
+
+                    }
+
+
+                }.start();
+
+                new CountDownTimer(2000,100){
+
+                    @Override
+                    public void onTick(long l) {
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        pressed_pos_2=-1;
+                        pressed_pos_1=-1;
+                        pressed_card_1=0;
+                        pressed_card_2=0;
+                        for(int i=0;i<8;i++){
+                            if(!opened_cards[i])
+                                viewing_cards[i].setEnabled(true);
+                        }
+                    }
+                }.start();
+
+            }
+
+
+
+        }
     }
+
+    public static void shuffle(int[] tempArr) {
+        Random rand = new Random();
+
+        for (int i = 0; i < tempArr.length; i++) {
+            int randomIndexToSwap = rand.nextInt(tempArr.length);
+            int temp = tempArr[randomIndexToSwap];
+            tempArr[randomIndexToSwap] = tempArr[i];
+            tempArr[i] = temp;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        opened_cards = new boolean[8];
         rand = new Random();
         player = findViewById(R.id.player);
+        score = findViewById(R.id.score);
         player.setText(getIntent().getStringExtra("player"));
         fetchCards();
         StartPlaying();
     }
-    public void reset(View v){
+
+    public void reset(View v) {
         StartPlaying();
     }
 }
